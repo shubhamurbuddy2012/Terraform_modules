2a8e43eb2d284ac7e157044f4d4f7ddfec1a6d7f
shubhamurbuddy2012 <shubham.official2012@gmail.com>
2026-08-16 01:44:44 +0530

    fix: move backend block inside terraform configuration

--- environments/preprod/provider.tf
+++ environments/preprod/provider.tf
@@ -2,8 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.81.0"
+      version = "~> 4.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateshubham2026"
+    container_name       = "tfstate"
+    key                  = "preprod.terraform.tfstate"
   }
 }
 
